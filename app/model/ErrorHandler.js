1181d817f3b962616fe493ebaea05596e52bde73
nicosammito <nico@sammito.de>
2021-02-26 09:12:19 +0100

    updated to Arrow Function and deleted empty lines

--- app/model/ErrorHandler.js
+++ app/model/ErrorHandler.js
@@ -1,5 +1,5 @@
 import {BaseError} from "./BaseError.js";
 
-window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
+window.onerror =  (myErrorHandler(errorMsg, url, lineNumber) => {
     throw new BaseError(errorMsg + " in line " +lineNumber + "<br>" + url);
 }
