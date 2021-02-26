1a884737eccf25c2626866d14f384522112fab4f
nicosammito <60352747+nicosammito@users.noreply.github.com>
2021-02-26 23:27:10 +0100

    added run and setChild function to BaseContainer and tested child

--- app/model/BaseContainer.js
+++ app/model/BaseContainer.js
@@ -4,8 +4,16 @@ import {SectionLoader} from "./SectionLoader.js"
 export class BaseContainer{
   
   child;
+  style;
   contentid = null;
-  
+
+  getTagName = "div";
+  getClassNames = ["container"];
+
+  /**
+   * constructor of BaseContainer
+   * @param {Object} obj
+   */
   constructor(obj) {
     
     if (obj === undefined) {
@@ -13,23 +21,47 @@ export class BaseContainer{
     }
     
     if(obj.child !== undefined){
-      this.child = obj.child;
-    }else{
-      throw new BaseError("Child is required!");
-    }
+      if(Array.isArray(obj.child)){
+        this.child = obj.child;
+      }else throw new BaseError("Child must be type of array!");
+    }else throw new BaseError("Child is required!");
+
+    /*if(obj.style !== undefined){
+      if(obj.style instanceof BaseStyle){
+
+      }else throw new BaseError("Style must be type of BaseStyle");
+    }*/
     
   }
-  
+
+  /**
+   * run's the BaseContainer
+   * @param c
+   * @return {Promise<void>}
+   */
   run = async (c) => {
     this.contentid = c;
+    await setChild(this);
   }
   
 }
 
+
+/**
+ * set's all child's underneath the container
+ * @param {Object} obj
+ * @return {Promise<boolean>}
+ */
 function setChild(obj){
   return new Promise(resolve => {
-    const node = document.getElementById(obj.contentid)[0];
+    const node = document.getElementById(obj.contentid) ;
     const sectionLoader = new SectionLoader();
-    obj.child.
+    obj.child.forEach((item) => {
+      sectionLoader.setSection(item).then((c) => {
+        node.appendChild(c);
+        item.run(c.id);
+        resolve(true);
+      })
+    })
   })
 }
